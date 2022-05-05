f9ca5ab1524aade951ba8e837050824b133cfab1
Anjali Nagpal <88620628+Anjali-5373@users.noreply.github.com>
2022-05-05 20:20:46 +0530

    Create areaofcircle.py

---
+++ areaofcircle.py
@@ -0,0 +1,5 @@
+# Let's make finding area of circle a bit intresting.
+import math
+radius=int(input("Enter radius for your pizza to know the area of your pizza \n"))
+area= math.pi*radius*radius
+print("Area for your pizza will be  ",area)
