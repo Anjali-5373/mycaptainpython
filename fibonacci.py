9a2514b11ec49c2d6f285d844d5b5b2e9d80b2cd
Anjali Nagpal <88620628+Anjali-5373@users.noreply.github.com>
2022-05-06 20:03:47 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+f=int(input("Enter the no. of finboncci elements you want"))
+f1=0
+f2=1
+if f==1:
+    print(f1)
+else:
+    print(f1)
+    print(f2)
+    for i in range(0,f):
+        f3=f1+f2
+        f1=f2
+        f2=f3
+        print(f3)
