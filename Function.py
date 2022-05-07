f2ceee684bb1fa6e2de28e2d46c46133dd184c93
Anjali Nagpal <88620628+Anjali-5373@users.noreply.github.com>
2022-05-07 19:41:46 +0530

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,11 @@
+def most_frequent(string):
+    d = dict()
+    for key in string:
+        if key not in d:
+            d[key] = 1
+        else:
+            d[key] += 1
+    return d
+
+s=str(input("Enter a string:"))
+print(most_frequent(s))
